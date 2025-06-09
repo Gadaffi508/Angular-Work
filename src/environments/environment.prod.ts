ec9569eb10a12149c22cd3bd6ae1a9cc2a36e32b
Yusuf <121219831+Gadaffi508@users.noreply.github.com>
2025-06-09 21:33:50 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseUrl : 'https://anket-uygulamasi-1fc60-default-rtdb.firebaseio.com/',
+  firebase: {
+  apiKey: "AIzaSyCna4ARQV5OiHw6SBANWwAKKQhUyPZ8akc",
+  authDomain: "anket-uygulamasi-1fc60.firebaseapp.com",
+  databaseURL: "https://anket-uygulamasi-1fc60-default-rtdb.firebaseio.com",
+  projectId: "anket-uygulamasi-1fc60",
+  storageBucket: "anket-uygulamasi-1fc60.firebasestorage.app",
+  messagingSenderId: "1002954642935",
+  appId: "1:1002954642935:web:86ab1b37aa6ff51c5f8af2"
+}
 };
